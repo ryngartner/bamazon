bf18e9533c989d1a366bf00d0d3c16c5a435b42b
Ryan Gartner <ryanjgartner@gmail.com>
2019-03-03 12:28:52 -0600

    Additional Content

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,30 +12,37 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id" + connection.threadId + "\n");
-    readBamazon();
+    start();
 });
 
-function readBamazon() {
+function start() {
     connection.query("SELECT * FROM products", function(err, res){
-        if (err) throw err;
-        
-        console.log(res);
+        if (err) throw err;   
+    inquirer
+    .prompt([
+        {
+        name: "id",
+        type: "rawlist",
+        choices: function() {
+            var idArray = [];
+            for (var i = 0; i < res.length; i++) {
+                idArray.push(res[i].product_name);
+            }
+            return idArray;
+        },
+        message: "What is the ID of the product you would like to buy?"
+    },
+    {
+        name: "quantity",
+        type: "input",
+        message: "How many would you like to purchase?"    
+    }
+    ])
+    .then(function(answers){
+        console.log(answers.id);
         connection.end();
     });
-
-}
-
-
+});   
+};
 
 
-// function userInput(){
-//     inquirer
-//     .prompt({
-//         name: "id",
-//         type: "input",
-//         message: "What is the ID of the product you would like to buy?"
-//     })
-//     .then(function(answer) {
-//         var id = 
-//     })
-// }
