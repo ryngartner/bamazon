9679e0fec387acda5b47b306abf87bec9229f50c
Ryan Gartner <ryanjgartner@gmail.com>
2019-03-06 16:35:32 -0600

    Working on getting UPDATE functioning

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,61 +9,61 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id" + connection.threadId + "\n");
     start();
 });
 
 function start() {
-    // console.table("SELECT * FROM products");
-    connection.query("SELECT * FROM products", function(err, res){
-        if (err) throw err;  
-        console.table(res); 
-    inquirer
-    .prompt([
-        {
-        name: "id",
-        type: "list",
-        choices: function() {
-            var idArray = [];
-            for (var i = 0; i < res.length; i++) {
-                idArray.push(res[i].item_id + " " + res[i].product_name);
-            }
-            return idArray;
-        },
-        message: "Select the ID of the product you would like to buy?"
-    },
-    {
-        name: "quantity",
-        type: "input",
-        message: "How many would you like to purchase?"    
-    }
-    ])
-    .then(function(answers){
-        console.log(answers.id);
-        console.log(answers.quantity);
-        var index = answers.id - 1;
-        var chosenItem = res[0]; 
-        var chosenItemQuantity = chosenItem.stock_quantity;
-        if(answers.quantity > chosenItemQuantity){
-            console.log("Insufficient Quantity!");
-        }
-        else {
-           var updatedQuantity = chosenItemQuantity - answers.quantity;
-           connection.query("UPDATE products SET ? WHERE ?",[{stock_quantity : updatedQuantity},
-            {item_id : index}], 
-            function(err, res) {
-                if (err) throw err;
-            })
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        inquirer
+            .prompt([{
+                    name: "id",
+                    type: "input",
+                    choices: function () {
+                        var idArray = [];
+                        for (var i = 0; i < res.length; i++) {
+                            idArray.push(res[i].item_id + " " + res[i].product_name);
+                        }
+                        return idArray;
+                    },
+                    message: "Select the ID of the product you would like to buy?"
+                },
+                {
+                    name: "quantity",
+                    type: "input",
+                    message: "How many would you like to purchase?"
+                }
+            ])
+            .then(function (answers) {
+                // console.log(answers.id);
+                // console.log(answers.quantity);
+                var index = answers.id - 1;
+                // console.log(index);
+                var chosenItem = res[index];
+                // console.log(chosenItem);
+                var chosenItemQuantity = chosenItem.stock_quantity;
+                // console.log(chosenItemQuantity);
+                if (answers.quantity > chosenItemQuantity) {
+                    console.log("Insufficient Quantity!");
+                } else {
+                    var updatedQuantity = chosenItemQuantity - answers.quantity;
+                    //    connection.query("UPDATE products SET ? WHERE ?",[{stock_quantity : updatedQuantity},
+                    //     {item_id : index}], 
+                    //     function(err, res) {
+                    //         if (err) throw err;
+                    var total = (res[index].price * answers.quantity).toFixed(2);
+                    console.log(res[index].price);
+                    console.log("The total cost of your purchase is: " + total);
+                // })
+                };
 
-        var total = res[0].price * answer.quantity;
-        console.log("The total cost of your purchase is: " + total);
 
-        }
-        connection.end();
+
+                connection.end();
+            });
     });
-});   
-}
-
-
+};
